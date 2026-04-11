78e0fc27f7bae7619055a34840162b0c1a42a6b5
Achinta Hazra <achintahazra8515@gmail.com>
2026-04-11 19:26:49 +0530

    s

--- ADMIN/app/layout.js
+++ ADMIN/app/layout.js
@@ -1,16 +1,20 @@
 import "./globals.css";
 import UpdateStatus from "@/components/UpdateStatus";
 import CustomDialog from "@/components/CustomDialog";
+import ServiceWorker from "../components/ServiceWorker";
 
 export const metadata = {
   title: "",
   description: "Secure Access Only",
+  manifest: "/manifest.json",
+  themeColor: "#000000",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <ServiceWorker /> 
         <CustomDialog />
         <UpdateStatus />
         {children}
